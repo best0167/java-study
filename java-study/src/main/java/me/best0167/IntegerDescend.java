77b810eded5d38ce3876774d6b59a0380abfb8b3
frame <best0167@naver.com>
2020-12-26 12:29:41 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/IntegerDescend.java
@@ -0,0 +1,17 @@
+package me.best0167;
+
+public class IntegerDescend {
+    public static long solution(long n) {
+        long answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        long n = 118372;
+
+        System.out.println(solution(n));
+    }
+
+}
