534c1d51621761476e0d2cc624d9102269f7986d
frame <best0167@naver.com>
2021-01-07 16:26:01 +0900

    오늘의 알고리즘 공부

--- java-study/src/main/java/me/best0167/IntegerDescend.java
+++ java-study/src/main/java/me/best0167/IntegerDescend.java
@@ -1,10 +1,43 @@
 package me.best0167;
 
+import java.util.*;
+
 public class IntegerDescend {
     public static long solution(long n) {
         long answer = 0;
+        int count = 0;
 
+        List<Long> list = new ArrayList<>();
 
+        while (n > 0) {
+            list.add(n % 10);
+            n /= 10;
+            count++;
+        }
+        Collections.sort(list);
+
+        for(int i = 0; i < count; i++) {
+            answer += list.get(i) * Math.pow(10, i);
+        }
+
+        /* long a = n;
+
+        while (a !=0) {
+            a /= 10;
+            count++;
+        }
+
+        List<Long> list = new ArrayList<>();
+
+        for(int i = 0; i < count; i++) {
+            list.add(n % 10);
+            n /= 10;
+        }
+        Collections.sort(list);
+
+        for(int i = 0; i < count; i++) {
+           answer += list.get(i) * Math.pow(10, i);
+        }*/
         return answer;
     }
 
