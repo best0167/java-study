a9cc73c77966b7ba16a386bf4e50d9f677b0920e
frame <best0167@naver.com>
2021-02-05 20:15:56 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/StockPrice.java
@@ -0,0 +1,22 @@
+package me.best0167;
+
+public class StockPrice {
+
+/*    public static int[] solution(int[] prices) {
+        Set<int> usedNumbers = new HashSet<>();
+
+        if(int turn=0; turn<prices.length; turn++) {
+            int usedNumbver
+        }
+        return null;
+    }*/
+
+    public static void main(String[] args) {
+
+        int[] prices = {1, 2, 3, 2, 3};
+
+
+        System.out.println(prices);
+
+    }
+}
