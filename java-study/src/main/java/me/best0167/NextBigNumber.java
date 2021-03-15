bc194dbaa5107d3359b9cc4464ceb65827731d97
frame <best0167@naver.com>
2021-03-15 23:13:06 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/NextBigNumber.java
@@ -0,0 +1,22 @@
+package me.best0167;
+
+public class NextBigNumber {
+    public static int solution(int n) {
+        int answer = 0;
+        int count = 0;
+
+        StringBuilder a = new StringBuilder();
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 78;
+        System.out.println(solution(n));
+
+        int n1 = 15;
+        System.out.println(solution(n1));
+    }
+}
