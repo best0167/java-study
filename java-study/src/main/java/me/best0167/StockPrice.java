db32701cd792c39f077ad38470a056d12116efd6
frame <best0167@naver.com>
2021-03-17 22:22:41 +0900

    오늘의 알고리즘 공부

--- java-study/src/main/java/me/best0167/StockPrice.java
+++ java-study/src/main/java/me/best0167/StockPrice.java
@@ -1,22 +1,33 @@
 package me.best0167;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class StockPrice {
 
-/*    public static int[] solution(int[] prices) {
-        Set<int> usedNumbers = new HashSet<>();
+    public static int[] solution(int[] prices) {
 
-        if(int turn=0; turn<prices.length; turn++) {
-            int usedNumbver
+        int count = prices.length;
+        int[] answer = new int[5];
+
+        for(int i = 0; i < count; i++) {
+            if(prices[i] >= i) {
+                answer[i] = count - i - 1;
+            } else {
+                answer[i] = 1;
+            }
         }
-        return null;
-    }*/
+
+        return answer;
+
+    }
 
     public static void main(String[] args) {
 
         int[] prices = {1, 2, 3, 2, 3};
 
-
-        System.out.println(prices);
+        System.out.println(Arrays.toString(prices));
 
     }
 }
