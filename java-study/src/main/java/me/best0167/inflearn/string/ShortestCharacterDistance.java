69317b724054e968494be39ae3c056554b36f406
frame <best0167@naver.com>
2021-05-13 23:45:25 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/string/ShortestCharacterDistance.java
@@ -0,0 +1,37 @@
+package me.best0167.inflearn.string;
+
+import java.util.Arrays;
+
+public class ShortestCharacterDistance {
+    public static int[] solution(String s, char t) {
+        int[] answer = new int[s.length()];
+        int p = 1000;
+        for(int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == t) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
+        }
+
+        p = 1000;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == t) {
+                p = 0;
+            } else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "teachermode";
+        char t = 'e';
+        System.out.println(Arrays.toString(solution(s, t)));
+    }
+}
