362b73fad71423183b04980f58617f9a8e39605d
frame <best0167@naver.com>
2021-05-15 23:56:39 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/string/Password.java
@@ -0,0 +1,16 @@
+package me.best0167.inflearn.string;
+
+public class Password {
+    public static String solution(String s) {
+        String answer = "";
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "#****###**#####**#####**##** ";
+        System.out.println(solution(s));
+    }
+
+}
