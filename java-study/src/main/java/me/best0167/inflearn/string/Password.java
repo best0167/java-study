7966507f912808b3dfbff39fcb17a0f8f3113df0
frame <best0167@naver.com>
2021-05-16 23:01:25 +0900

    오늘의 알고리즘 공부

--- java-study/src/main/java/me/best0167/inflearn/string/Password.java
+++ java-study/src/main/java/me/best0167/inflearn/string/Password.java
@@ -1,16 +1,28 @@
 package me.best0167.inflearn.string;
 
 public class Password {
-    public static String solution(String s) {
-        String answer = "";
+    public static String solution(int n, String s) {
+        StringBuilder answer = new StringBuilder();
+        int share = s.length() / n;
 
+        for(int i = 0; i < n; i++) {
+            String tmp = s.substring(0, share).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            System.out.println(tmp + " " + num);
+            answer.append((char) num);
 
-        return answer;
+            s = s.substring(share);
+
+        }
+
+        return answer.toString();
     }
 
     public static void main(String[] args) {
+        int n = 4;
         String s = "#****###**#####**#####**##** ";
-        System.out.println(solution(s));
+
+        System.out.println(solution(n, s));
     }
 
 }
