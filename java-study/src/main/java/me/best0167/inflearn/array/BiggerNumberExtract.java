cea2876ad880aeeb1b1a192fdc1eb4f6bf8ef93c
frame <best0167@naver.com>
2021-05-17 23:24:36 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/array/BiggerNumberExtract.java
@@ -0,0 +1,21 @@
+package me.best0167.inflearn.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static me.best0167.GreedyPractice.n;
+
+public class BiggerNumberExtract {
+    public static List<Integer> solution(int n, int[] array) {
+        List<Integer> answer = new ArrayList<>();
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "it is time to study";
+        int[] array = {7, 3, 9, 5, 6, 12};
+        System.out.println(solution(n, array));
+    }
+}
