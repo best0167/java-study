1acd975bce6862c18bfa85f61e0ffd4e6cee6d44
frame <best0167@naver.com>
2021-05-19 23:10:18 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/array/Watchedstudent.java
@@ -0,0 +1,25 @@
+package me.best0167.inflearn.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Watchedstudent {
+    public static int solution(int n, int[] array) {
+        int answer = 1;
+        int max = array[0];
+
+        for (int i = 0; i < n; i++) {
+            if (array[i] > max) {
+                answer++;
+                max = array[i];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 8;
+        int[] array = {130, 135, 148, 140, 145, 150, 150, 153};
+        System.out.println(solution(n, array));
+    }
+}
