f9d21ea5b5a3d861a8e9509f586bd5169d918ef8
frame <best0167@naver.com>
2021-05-28 16:51:38 +0900

    오늘의 알고리즘 공부

--- java-study/src/main/java/me/best0167/inflearn/array/TemporaryClassMonitor.java
+++ java-study/src/main/java/me/best0167/inflearn/array/TemporaryClassMonitor.java
@@ -2,13 +2,24 @@ package me.best0167.inflearn.array;
 
 public class TemporaryClassMonitor {
     public static int solution(int n, int[][] array) {
-        int answer = 0;
+        int answer = 0, max = Integer.MIN_VALUE;
 
         for(int i = 0; i < n; i++) {
+            int cnt = 0;
             for (int j = 0; j < n; j++) {
-
+                for (int k = 0; k < 5; k++) {
+                    if (array[i][k] == array[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if(cnt > max) {
+                max = cnt;
+                answer = i + 1;
             }
         }
+
 
         return answer;
     }
@@ -16,10 +27,10 @@ public class TemporaryClassMonitor {
     public static void main(String[] args) {
         int n = 5;
         int[][] array = {{2, 3, 1, 7, 3},
-                {4, 1, 9, 6, 8},
-                {5, 5, 2, 4, 4},
-                {6, 5, 2, 6, 7},
-                {8, 4, 2, 2, 2}};
+                        {4, 1, 9, 6, 8},
+                        {5, 5, 2, 4, 4},
+                        {6, 5, 2, 6, 7},
+                        {8, 4, 2, 2, 2}};
         System.out.println(solution(n, array));
     }
 }
