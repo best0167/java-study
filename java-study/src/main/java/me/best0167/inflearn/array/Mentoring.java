4758215c2d7484508bf2990272642c89fe153054
frame <best0167@naver.com>
2021-05-29 23:20:31 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/array/Mentoring.java
@@ -0,0 +1,17 @@
+package me.best0167.inflearn.array;
+
+public class Mentoring {
+    public static int solution(int n) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 20;
+
+        System.out.println(solution(n));
+    }
+
+}
