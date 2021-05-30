4e02b38884e1041c01e75480da9b915a6ded833d
frame <best0167@naver.com>
2021-05-30 23:28:23 +0900

    오늘의 알고리즘 공부

--- java-study/src/main/java/me/best0167/inflearn/array/Mentoring.java
+++ java-study/src/main/java/me/best0167/inflearn/array/Mentoring.java
@@ -1,7 +1,7 @@
 package me.best0167.inflearn.array;
 
 public class Mentoring {
-    public static int solution(int n) {
+    public static int solution(int[] n, int[][] array) {
         int answer = 0;
 
 
@@ -9,9 +9,11 @@ public class Mentoring {
     }
 
     public static void main(String[] args) {
-        int n = 20;
-
-        System.out.println(solution(n));
+        int[] n = {4, 3};
+        int[][] array = {{3, 4, 1, 2},
+                        {4, 3, 2, 1},
+                        {3, 1, 4, 2}};
+        System.out.println(solution(n, array));
     }
 
 }
