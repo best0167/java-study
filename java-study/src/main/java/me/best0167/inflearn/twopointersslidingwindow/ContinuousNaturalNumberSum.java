f5b3914ff5499bb2fac156a561155d2058687c66
frame <best0167@naver.com>
2021-06-05 22:48:29 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/twopointersslidingwindow/ContinuousNaturalNumberSum.java
@@ -0,0 +1,18 @@
+package me.best0167.inflearn.twopointersslidingwindow;
+
+public class ContinuousNaturalNumberSum {
+    public static int solution(int n) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 15;
+
+        System.out.println(solution(n));
+    }
+
+}
