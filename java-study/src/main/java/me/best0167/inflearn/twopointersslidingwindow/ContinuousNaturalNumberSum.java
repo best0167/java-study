24e681ad4430d8036a34675421d5405a1975d6cd
frame <best0167@naver.com>
2021-06-06 22:39:04 +0900

    오늘의 알고리즘 공부

--- java-study/src/main/java/me/best0167/inflearn/twopointersslidingwindow/ContinuousNaturalNumberSum.java
+++ java-study/src/main/java/me/best0167/inflearn/twopointersslidingwindow/ContinuousNaturalNumberSum.java
@@ -2,9 +2,24 @@ package me.best0167.inflearn.twopointersslidingwindow;
 
 public class ContinuousNaturalNumberSum {
     public static int solution(int n) {
-        int answer = 0;
+        int answer = 0, sum = 0;
+        int m = n / 2+1;
+        int[] arr = new int[m];
+        for(int i=0; i<m; i++) {
+            arr[i]=i+1;
+        }
 
-
+        int lt=0;
+        for(int rt=0; rt<m; rt++){
+            sum+=arr[rt];
+            if(sum==n) {
+                answer++;
+            }
+            while(sum>=n){
+                sum-=arr[lt++];
+                if(sum==n) answer++;
+            }
+        }
 
         return answer;
     }
