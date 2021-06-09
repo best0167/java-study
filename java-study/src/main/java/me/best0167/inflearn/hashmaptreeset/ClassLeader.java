c41113bcd23c975c94bb09e83322a8dfb2c63e0e
frame <best0167@naver.com>
2021-06-09 22:39:29 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/hashmaptreeset/ClassLeader.java
@@ -0,0 +1,18 @@
+package me.best0167.inflearn.hashmaptreeset;
+
+public class ClassLeader {
+    public static String solution(int n, String s) {
+        String answer = "";
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 15;
+        String s = "BACBACCACCBDEDE";
+
+        System.out.println(solution(n, s));
+    }
+
+}
