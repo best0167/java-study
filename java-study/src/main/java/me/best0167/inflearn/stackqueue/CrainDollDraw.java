33ba0545c2df292f7e4dc69061d979374bbd18bd
frame <best0167@naver.com>
2021-06-18 23:11:14 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/stackqueue/CrainDollDraw.java
@@ -0,0 +1,37 @@
+package me.best0167.inflearn.stackqueue;
+
+import java.util.Stack;
+
+public class CrainDollDraw {
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int pos : moves) {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][pos - 1] != 0) {
+                    int tmp = board[i][pos - 1];
+                    board[i][pos - 1] = 0;
+                    if (!stack.isEmpty() && tmp == stack.peek()) {
+                        answer += 2;
+                        stack.pop();
+                    } else {
+                        stack.push(tmp);
+                    }
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {{0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 3},
+                {0, 2, 5, 0, 1},
+                {4, 2, 4, 4, 2},
+                {3, 5, 1, 3, 1}};
+        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
+
+        System.out.println(solution(board, moves));
+    }
+}
