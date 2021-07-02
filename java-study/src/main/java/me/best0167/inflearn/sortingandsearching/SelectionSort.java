12ccdb09d847cfd4ec2a8d730c81f1fe2001bcac
frame <best0167@naver.com>
2021-07-02 17:09:17 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/sortingandsearching/SelectionSort.java
@@ -0,0 +1,18 @@
+package me.best0167.inflearn.sortingandsearching;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static int[] solution(int n, int[] array) {
+        int[] answer = new int[n];
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[] array = {13, 5, 11, 7, 23, 15};
+
+        System.out.println(Arrays.toString(solution(n, array)));
+    }
+}
