822433a01ed129db640feb3d4ecf36fe2dc597b9
frame <best0167@naver.com>
2021-07-05 23:43:49 +0900

    오늘의 알고리즘 공부

--- java-study/src/main/java/me/best0167/inflearn/sortingandsearching/BubbleSort.java
+++ java-study/src/main/java/me/best0167/inflearn/sortingandsearching/BubbleSort.java
@@ -1,8 +1,22 @@
 package me.best0167.inflearn.sortingandsearching;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static int[] solution(int n, int[] array) {
         int[] answer = new int[n];
+
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-i-1; j++){
+                if(array[j] > array[j+1]){
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
+            }
+        }
+
+        answer = array;
 
         return answer;
     }
@@ -10,6 +24,8 @@ public class BubbleSort {
     public static void main(String[] args) {
         int n = 6;
         int[] array = {13, 5, 11, 7, 23, 15};
+        System.out.println(Arrays.toString(solution(n, array)));
+
     }
 
 
