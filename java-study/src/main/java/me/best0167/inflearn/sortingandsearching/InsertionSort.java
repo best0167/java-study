018854ba3422868b8b15386b4268e1fbeec5d84a
frame <best0167@naver.com>
2021-07-06 23:15:48 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/sortingandsearching/InsertionSort.java
@@ -0,0 +1,19 @@
+package me.best0167.inflearn.sortingandsearching;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static int[] solution(int n, int[] array) {
+        int[] answer = {};
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[] array = {11, 7, 5, 6, 10, 9};
+
+        System.out.println(Arrays.toString(solution(n, array)));
+
+    }
+}
