ef64695f6182eb2a24890299b9c246b94078484b
frame <best0167@naver.com>
2021-07-07 22:17:38 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/sortingandsearching/LeastRecentlyUsed.java
@@ -0,0 +1,19 @@
+package me.best0167.inflearn.sortingandsearching;
+
+import java.util.Arrays;
+
+public class LeastRecentlyUsed {
+    public static int[] solution(int n, int m, int[] array) {
+        int[] answer = {};
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int m = 9;
+        int[] array = {1, 2, 3, 2, 6, 2, 3, 5, 7};
+
+        System.out.println(Arrays.toString(solution(n, m, array)));
+    }
+}
