0e4f05093b2f107ab3112354eadc01dd03277e6a
frame <best0167@naver.com>
2021-07-10 23:46:03 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/sortingandsearching/DoubleCheck.java
@@ -0,0 +1,16 @@
+package me.best0167.inflearn.sortingandsearching;
+
+public class DoubleCheck {
+    public static String solution(int n, int[] array) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 8;
+        int[] array = {20, 25, 52, 30, 39, 33, 43, 33};
+
+        System.out.println(solution(n, array));
+    }
+}
