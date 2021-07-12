8f6684c4780cc8c03910076befc594d98ceb77cb
frame <best0167@naver.com>
2021-07-12 23:13:15 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/sortingandsearching/MisChief.java
@@ -0,0 +1,18 @@
+package me.best0167.inflearn.sortingandsearching;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MisChief {
+    public static List<Integer> solution(int n, int[] array) {
+        List<Integer> answer = new ArrayList<>();
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 9;
+        int[] array = {120, 125, 152, 130, 135, 135, 143, 127, 160};
+        System.out.println(solution(n, array));
+    }
+}
