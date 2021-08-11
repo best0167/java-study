8a42bdf4b64c4720e9821e2ee01e54c527c48b9d
frame <best0167@naver.com>
2021-08-11 23:18:39 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/stackqueue/BinarySearch.java
@@ -0,0 +1,19 @@
+package me.best0167.inflearn.stackqueue;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static int[] solution(int n, int[] array) {
+        int[] answer = new int[n];
+
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        int n = 8;
+        int[] array = {23, 87, 65, 12, 57, 32, 99, 81};
+
+        System.out.println(Arrays.toString(solution(n, array)));
+    }
+}
