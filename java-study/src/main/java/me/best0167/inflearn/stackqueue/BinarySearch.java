e9f0bfd94c7a04649bc023bbe1956d7d2ac9dc03
frame <best0167@naver.com>
2021-08-12 23:59:07 +0900

    오늘의 알고리즘 공부

--- java-study/src/main/java/me/best0167/inflearn/stackqueue/BinarySearch.java
+++ java-study/src/main/java/me/best0167/inflearn/stackqueue/BinarySearch.java
@@ -3,8 +3,25 @@ package me.best0167.inflearn.stackqueue;
 import java.util.Arrays;
 
 public class BinarySearch {
-    public static int[] solution(int n, int[] array) {
-        int[] answer = new int[n];
+    public static int solution(int n, int m, int[] array) {
+        int answer = 0;
+
+        Arrays.sort(array);
+        int lt = 0, rt = n-1;
+
+        while(lt <= rt){
+            int mid = (lt + rt)/2;
+            if(array[mid] == m){
+                answer = mid + 1;
+                break;
+            }
+            if(array[mid] > m) {
+                rt = mid - 1;
+            }
+            else {
+                lt = mid + 1;
+            }
+        }
 
         return answer;
 
@@ -12,8 +29,9 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int n = 8;
+        int m = 32;
         int[] array = {23, 87, 65, 12, 57, 32, 99, 81};
 
-        System.out.println(Arrays.toString(solution(n, array)));
+        System.out.println((solution(n, m, array)));
     }
 }
