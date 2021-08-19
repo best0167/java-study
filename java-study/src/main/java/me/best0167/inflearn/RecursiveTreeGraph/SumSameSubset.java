8caa9a55eb0b17a2aae59ac66198816e4c71f13c
frame <best0167@naver.com>
2021-08-19 22:50:38 +0900

    오늘의 알고리즘 공부

---
+++ java-study/src/main/java/me/best0167/inflearn/RecursiveTreeGraph/SumSameSubset.java
@@ -0,0 +1,16 @@
+package me.best0167.inflearn.RecursiveTreeGraph;
+
+public class SumSameSubset {
+    public static String solution(int n, int[] array) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[] array = {1, 3, 5, 6, 7, 10};
+
+        System.out.println(solution(n, array));
+    }
+}
